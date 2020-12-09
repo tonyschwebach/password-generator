bf44b58409ac495aa375be2e040896000d3d1b96
T Schway <tony.schwebach@gmail.com>
2020-12-09 17:36:10 -0500

    added prompts, and logic to add arrays to possible characters array

--- script.js
+++ script.js
@@ -12,22 +12,26 @@ function writePassword() {
 
 // define function for generating random password 
 function generatePassword(){
-// blank array for possible characters
+  // blank array for possible characters
   var possibleCharacters = [];
+  var securePassword = "";
   
   // create arrays for each of the acceptable characters
+  var includeLower = true;
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-  var upperCase = []; // loop over lower case letters to make an array of upper case letters so I don't have to type them again 
-  for(i=0; i<lowerCase.length; i++) {
-    upperCase.push(lowerCase[i].toUpperCase());
-  }
-  var numbericChars = ["0","1","2","3","4","5","6","7","8","9"]; //list as string as password should be string type
+  var includeUpper = true;
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  
+  var includeNumeric = true;
+  var numericChars = ["0","1","2","3","4","5","6","7","8","9"]; //list numbers as string as password should be string type
+
+  var includeSpecial = true;
   var specialChars = ["!","@","#","$","%","^","&","*","(",")"]; // list of special characters in string type
 
   // prompt user for variable for password length 8-128 characters
-  var passwordLength = 0;
-  // console.log(typeof passwordLength);
+  var passwordLength = 12;
+
 
   // while (passwordLength < 8 || passwordLength >128 || typeof passwordLength !== "number") {
   //   var passwordLength = prompt("How many characters long would you like your password? Please enter a number between 8 and 128.");
@@ -40,22 +44,62 @@ function generatePassword(){
 
 
 
+  // define function to add each array of characters to the list of possible password characters 
+  function addCharsToPossible(array) {
+    for (var j = 0; j < array.length; j++) {
+      possibleCharacters.push(array[j]);
+    }
+  }
+
+  
+// prompt user to include each array of characters
+  includeLower = confirm("Include lower case letters?");
+  if(includeLower){
+    addCharsToPossible(lowerCase);
+  }
+
+  includeUpper = confirm("Include upper case letters?");
+  if(includeUpper){
+    addCharsToPossible(upperCase);
+  }
+
+  includeNumeric = confirm("Include numbers?");
+  if(includeNumeric){
+    addCharsToPossible(numericChars);
+  }
+
+  includeSpecial = confirm("Include special characters?");
+  if(includeSpecial){
+    addCharsToPossible(specialChars);
+  }
+
+  // feedback to user if they do not include any of the character arrays 
+  if(includeLower===false && includeUpper ===false && includeNumeric ===false && includeSpecial ===false){
+    alert("You must use characters in your password. Please click ok on at least one character group to generate another password.");
+    return "You must use characters in your password. Please click ok on at least one character group to generate another password.";
+  }
+
+  
 
 
-// prompt user to include each array of special characters
-// push array of chars to possible array
-// for loop over possible array for password length number of times
-// randomly select letter and add to new password
+  // for loop over array of possible characters for password length -3 number of times. last 3 characters will be reserved to ensure each requested character type is used
+  for(var j = 0; j < passwordLength-3; j++) {
+    var randomLetter = possibleCharacters[Math.floor(Math.random()*(possibleCharacters.length))]; //generate random character from possible characters list]
+    securePassword = securePassword + randomLetter;
+  }
+  console.log(securePassword);
+
+  // if(includeLower) {
+  //   for(var j = 0; j < securePassword.length; j++){
+  //     securePassword
+  //   }
+  // }
 
 
 
 
-
-
-
-
-
-  return "t3mp p@SSw0rD";
+  // return randomly generated password from this generatePassword function
+  return securePassword;
 }
 
 // Add event listener to generate button
